f9bd84a98b39d18a4c0fb6064226d8d0c98fbe4f
Karan <kv4020057@gmail.com>
2025-06-30 10:52:47 +0530

    All file are done

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,30 @@
 /**
  * @license MIT
- * @copyright codewithsadee 2023 All rights reserved
- * @author codewithsadee <mohammadsadee24@gmail.com>
+ * @copyright KaranVerma 2025 All rights reserved
+ * @author KaranVerma <karan@gmail.com>
  */
+
+'use strict';
+
+import { fetchData, url } from "./api.js";
+import * as module from "./module.js";
+
+/**
+ * Add event listener on multiple elements
+ * @param {Nodelist} elements Elements node array
+ * @param {string} eventType Event type e.g.: "click", "mouseover"
+ * @param {Function} callback Callback function
+ */
+const addEventOnElements = function (elements, eventType, callback) {
+    for (const element of elements) element, addEventListener(eventType, callback);
+}
+
+
+/**
+ * Toggle search in mobile devices
+ */
+const searchView = document.querySelector("[data-search-view]");
+const searchTogglers = document.querySelectorAll("[data-search-toggler]");
+
+const toggleSearch = () => searchView.classList.toggle("active");
+addEventOnElements(searchTogglers, "click", toggleSearch);
