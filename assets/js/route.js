a9e2d98a90d8433ab8cdd2e0a995be5d5f6779b9
Karan <kv4020057@gmail.com>
2025-07-11 19:17:02 +0530

    All error is getting in the app.js file

--- assets/js/route.js
+++ assets/js/route.js
@@ -7,8 +7,8 @@
 
 'use strict';
 
-import { updateWeather, error404 } from "./app.js";
-const defaultLocation = "#/weather?lat=51.5073219&lon=-0.127464"    // London
+import { updateWeather, error404 } from "./app.js"; 
+const defaultLocation = "#/weather?lat=51.5073219&lon=-0.1276474"    // London
 
 const currentLocation = function () {
     window.navigator.geolocation.getCurrentPosition(res => {
@@ -36,7 +36,7 @@ const checkHash = function () {
 
     const [route, query] = requestURL.includes ? requestURL.split("?") : [requestURL];
 
-    route.get(route) ? routes.get(route)(query) : error404();
+    routes.get(route) ? routes.get(route)(query) : error404();
 }
 
 window.addEventListener("hashchange", checkHash);
